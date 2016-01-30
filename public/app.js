8f4339f78ae4f9b15656181bab6b9b55cebcb2ae
yk0724 <yk0724@gmail.com>
2016-01-29 22:09:59 -0500

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,21 @@
+var jetbrains = angular.module("jetbrains", []);
+
+jetbrains.controller("AppCtrl", function ($http){
+    var app = this;
+    var url = "http://localhost:3000";
+
+    app.saveProduct = function(newProduct){
+        $http.post(url + "/add", {name: newProduct}).success(function() {
+            loadProducts();
+        })
+    }
+
+
+    function loadProducts() {
+        $http.get(url).success(function (products) {
+            app.products = products;
+        })
+    }
+
+    loadProducts();
+})
---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser());
+
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/jetbrains');
+
+var Product = mongoose.model('Product', {name: String});
+
+app.get("/", function(req, res){
+    Product.find(function(err, products){
+        res.send(products);
+    })
+})
+
+app.post("/add", function (req, res) {
+    var name = req.body.name;
+    var product = new Product({name: name});
+    product.save(function (err){
+        res.send();
+    })
+
+})
+app.listen(3000);
